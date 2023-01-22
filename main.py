b27453d9a9a4a6cf2e68d17599c1dc1ede74ff64
T306 <57376135+T306@users.noreply.github.com>
2023-01-22 00:48:12 +0000

    Did Stuff

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ app = Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 @app.route('/')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('pages/home.html', title='Home')
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About')
+    return render_template('pages/about.html', title='About')
 @app.route('/donate')
 def donate():
-    return render_template('donate.html', title='Donate')
+    return render_template('pages/donate.html', title='Donate')
 @app.route('/projects')
 def projects():
-    return render_template('projects.html', title='Projects')
+    return render_template('pages/projects.html', title='Projects')
+  
+                ### Errors ###
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
 
 app.run(host='0.0.0.0', port=81)
